53a13fc42c1ef611e5d9f665d5b3a6eeee37e3de
23hh <lsh6166@gmail.com>
2024-02-05 23:06:08 +0900

    追加機能実装

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const oneClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素設定
+  div.appendChild(p);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document.getElementById("add-button").addEventListener("click", oneClickAdd);
